913e891c627b05a7b13cc49663bfe1f6c7f9882c
Diego <diegojaraba02@gmail.com>
2026-01-05 02:44:09 +0100

    Add filtering of alumnos by gender selection

--- src/app/alumnos/alumno-listado/alumnos-listado.ts
+++ src/app/alumnos/alumno-listado/alumnos-listado.ts
@@ -10,14 +10,19 @@ import { AlumnosCount } from '../alumnos-count/alumnos-count';
   styleUrl: './alumnos-listado.css'
 })
 export class AlumnosListado {
-    alumnos: any[] = [
-      {nombre: 'Tadej', apellidos: 'Pogacar', direccion: 'Klanec, Eslovenia', fnac: '9/21/1998', sexo: 'Hombre' },
-      {nombre: 'Primoz', apellidos: 'Roglic', direccion: 'Trbovlje, Eslovenia', fnac: '10/29/1989', sexo: 'Hombre' },
-      {nombre: 'Wout', apellidos: 'Van Aert', direccion: 'Herentals, Bélgica', fnac: '9/15/1994', sexo: 'Hombre' },
-      {nombre: 'Marianne', apellidos: 'Vos', direccion: 's-Hertogenbosch, Países Bajos', fnac: '5/13/1987', sexo: 'Mujer' },
-      {nombre: 'Mathieu', apellidos: 'Van der Poel', direccion: 'Kapellen, Bélgica', fnac: '1/19/1995', sexo: 'Hombre' }
+  seleccion: string = 'Todos';
+  
+  alumnos: any[] = [
+    {nombre: 'Tadej', apellidos: 'Pogacar', direccion: 'Klanec, Eslovenia', fnac: '9/21/1998', sexo: 'Hombre' },
+    {nombre: 'Primoz', apellidos: 'Roglic', direccion: 'Trbovlje, Eslovenia', fnac: '10/29/1989', sexo: 'Hombre' },
+    {nombre: 'Wout', apellidos: 'Van Aert', direccion: 'Herentals, Bélgica', fnac: '9/15/1994', sexo: 'Hombre' },
+    {nombre: 'Marianne', apellidos: 'Vos', direccion: 's-Hertogenbosch, Países Bajos', fnac: '5/13/1987', sexo: 'Mujer' },
+    {nombre: 'Mathieu', apellidos: 'Van der Poel', direccion: 'Kapellen, Bélgica', fnac: '1/19/1995', sexo: 'Hombre' }
   ]
 
+  alCambiar(opcionElegida: string){
+    this.seleccion = opcionElegida;
+  }
   getTodos(): number {
     return this.alumnos.length;
   }
--- src/app/alumnos/alumnos-count/alumnos-count.ts
+++ src/app/alumnos/alumnos-count/alumnos-count.ts
@@ -1,14 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-alumnos-count',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './alumnos-count.html',
   styleUrl: './alumnos-count.css',
 })
 export class AlumnosCount {
   opcionElegida: string = 'Todos';
+
+  @Output() globalElegido: EventEmitter<string> = new EventEmitter<string>();
+
   @Input() todos: number = 0;
   @Input() hombres: number = 0;
   @Input() mujeres: number = 0;
+
+  cuandoCambiemos(){
+    this.globalElegido.emit(this.opcionElegida);
+  }
 }
